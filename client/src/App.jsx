9b574897353a71337ca76ce768b3f2f5101d2b4b
Garegin Hakobyan <garik.hakobyan.2004@gmail.com>
2023-05-25 23:27:32 +0400

    Changed UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,6 @@ export default function App() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
         const apiUrl = `http://localhost:3000/api/convert?input=${formData.input}`;
 
         fetch(apiUrl, {
@@ -24,45 +23,34 @@ export default function App() {
             .then(response => response.json())
             .then(data => {
                 setResult(data.string || data);
-                setJsonResult(JSON.stringify(data));
+                setJsonResult(JSON.stringify(data, null, 2));
             })
             .catch(error => console.error(error));
     };
 
     return (
-        <div>
-            <header>
+        <div className="wrapper">
+
+            <header className="header">
                 <h1>Metric/Imperial Converter</h1>
             </header>
-
-            <section>
-                <h3>Example usage</h3>
-                <code>/api/convert?input=4gal</code><br />
-                <code>/api/convert?input=1/2km</code><br />
-                <code>/api/convert?input=5.4/3lbs</code><br />
-                <code>/api/convert?input=kg</code><br />
-                <h3>Example return</h3>
-                <code>{'{ initNum: 3.1, initUnit: \'mi\', returnNum: 4.98895, returnUnit: \'km\', string: \'3.1 miles converts to 4.98895 kilometers\' }'}</code>
-            </section>
-
-            <section>
-                <div id="testui">
-                    <h3 style={{ textAlign: 'left' }}>Front-End</h3>
-                    <form id="convertForm" className="border" onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            className="convertField"
-                            name="input"
-                            placeholder="3.1mi"
-                            onChange={handleChange}
-                            value={formData.input}
-                        />
-                        <button>Convert!</button>
-                    </form>
-                    <p id="result">{result}</p>
-                    <code id="jsonResult">{jsonResult}</code>
+            <main>
+                <form className="form" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="convertField"
+                        name="input"
+                        placeholder="3.1mi"
+                        onChange={handleChange}
+                        value={formData.input}
+                    />
+                    <button>Convert!</button>
+                </form>
+                <p className="result">{result}</p>
+                <div className="jsonResult">
+                    <code>{jsonResult}</code>
                 </div>
-            </section>
+            </main>
 
         </div>
     );
